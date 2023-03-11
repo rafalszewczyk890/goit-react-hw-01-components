796004babdc3e2944f686bcddddab5636153055b
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-03-11 11:18:07 +0100

    Basic markup for profile and statistics done

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,14 @@
+export const Statistics = ({ title, stats }) => (
+  <section class="statistics">
+    <h2 class="title">{title}</h2>
+
+    <ul class="stat-list">
+      {stats.map(stat => (
+        <li class="item">
+          <span class="label">{stat.label}</span>
+          <span class="percentage">{stat.percentage}</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
